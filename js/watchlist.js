3625b2308ca80a5886c13b3002bf928753233136
Pulkit <kpulkit15234@gmail.com>
2023-06-28 10:24:46 +0530

    Implemented the dark mode toggle in the watchlist page

--- js/watchlist.js
+++ js/watchlist.js
@@ -1,3 +1,21 @@
+const toggle = document.getElementById('toggleDark');
+const body = document.querySelector('body');
+
+toggle.addEventListener('click', function () {
+    this.classList.toggle('bi-moon');
+    if (this.classList.toggle('bi-brightness-high-fill')) {
+        body.style.background = '#FFF4F4';
+        body.style.color = '#374259';
+        body.style.transition = '2s';
+
+
+    } else {
+        body.style.background = '#374259';
+        body.style.color = '#FFF4F4';
+        body.style.transition = '2s';
+
+    }
+});
 const movieResults = document.querySelector('.results')
 if(!localStorage.getItem('Watchlist')){
     localStorage.setItem('Watchlist', JSON.stringify([]))
